df1ae2737867523ea6196dbbd935d4552f74ba59
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-13 11:11:24 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  varidates :nick_name, presence: true
-  varidates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
-  varidates :last_name, presence: true, 
+  validates :nick_name, presence: true
+  validates :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :last_name, presence: true, 
             format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
-  varidates :first_name, presence: true, 
+  validates :first_name, presence: true, 
             format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
-  varidates :last_name_kana, presence: true,
+  validates :last_name_kana, presence: true,
             format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
-  varidates :first_name_kana, presence:true,
+  validates :first_name_kana, presence:true,
             format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
-  varidates :birthday: presence: true
+  validates :birthday, presence: true
 end
