d5a12029f03478718f0ec4510f29d68741ee7d39
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-14 09:54:15 +0900

    with_options実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, presence: true
-  validates :password, format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
-    validates :last_name, presence: true
-    validates :first_name, presence: true
+  
+  with_options presence: true do
+    validates :nick_name
+    validates :birthday
+    with_options format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}\z/ } do
+      validates :password, length: { minimum: 6 }
+    end
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence:true
-  end
-  validates :birthday, presence: true
 end
