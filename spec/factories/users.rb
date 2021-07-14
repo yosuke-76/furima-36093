dc7d217071794bfdfa56cfe4e419d0f3ae7809f5
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-14 11:44:46 +0900

    password_confirmationの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     nick_name {'aaa'}
     email {Faker::Internet.unique.free_email}
     password {'aaaaaa1'}
+    password_confirmation { password }
     last_name {'山田'}
     first_name {'太郎'}
     last_name_kana {'ヤマダ'}
