e9fa29382095f6e68157f06d1576491390e36f0a
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-16 21:05:19 +0900

    依頼事項修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :user_product, only: [:edit, :update]
   def index
     @products = Product.order('created_at DESC')
   end
@@ -9,24 +11,17 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to products_path
+      redirect_to root_path
     else
       render :new
     end
   end
   def show
-    @product = Product.find(params[:id])
   end
   def edit
-    @product = Product.find(params[:id])
-    unless user_signed_in? && current_user.id == @product.user_id
-      redirect_to "/"
-    end
   end
   def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    if @product.save
+    if @product.update(product_params)
       redirect_to product_path
     else
       render :edit
@@ -36,5 +31,13 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:image, :product_name, :product_text, :category_id, :product_status_id, :delivery_charge_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  def user_product
+    unless user_signed_in? && current_user.id == @product.user_id
+      redirect_to root_path
+    end
   end
 end
