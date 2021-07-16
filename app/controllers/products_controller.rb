6191f5da774ea21b4e0527481752b92d61497491
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-16 14:02:10 +0900

    変数の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @product = Product.all.order('created_at DESC')
+    @products = Product.order('created_at DESC')
   end
   def new
     @product = Product.new
