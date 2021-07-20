4ed4902b2b6c44eb8ef43311af5deb82e36d640b
yosuke-76 <85981665+yosuke-76@users.noreply.github.com>
2021-07-20 16:43:04 +0900

    補足修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: [:index, :create]
-  before_action :move_to_root, only: :index
+  before_action :move_to_root, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new
